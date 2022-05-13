eb907d1def3ca06d05753eae50aa9d0f86d31121
begalievn <begaliev.info@gmail.com>
2022-05-13 20:01:02 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import CarouselSlider from "./components/CarouselSlider";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CarouselSlider>
+        <img
+          className="image"
+          src="https://media.istockphoto.com/photos/exterior-view-of-modern-apartment-building-offering-luxury-rental-in-picture-id1322575582?b=1&k=20&m=1322575582&s=170667a&w=0&h=bGCtLpgCEorQuVdW2lbWguNZHcOGPePSwDibgbgyh0U="
+          alt="buildings"
+        />
+        <img
+          className="image"
+          src="https://media.istockphoto.com/photos/door-to-office-picture-id178842131?b=1&k=20&m=178842131&s=170667a&w=0&h=45iCO1nO7rvQS8NITsFmh4XQ_nIJfZ3-MVyP82ythdg="
+          alt="building"
+        />
+        <img
+          className="image"
+          src="https://images.unsplash.com/photo-1497366811353-6870744d04b2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YnVpbGRpbmdzJTIwZm9yJTIwb2ZmaWNlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
+          alt="building"
+        />
+        <div className="image-container">
+          <img
+            className="image"
+            src="https://media.istockphoto.com/photos/office-building-with-blue-windows-picture-id133906722?b=1&k=20&m=133906722&s=170667a&w=0&h=aHwRFIq-qKP7lkdfuHhQof0EVSr6KD5WOvotB5RXHwA="
+            alt="building"
+          />
+          <p className="text">Some text </p>
+        </div>
+        <img
+          className="image"
+          src="https://media.istockphoto.com/photos/modern-white-office-entrance-door-picture-id1166218092?b=1&k=20&m=1166218092&s=170667a&w=0&h=ur-gKb50JRfBMahKo9AgZ5GtXY1H-lIZIq4WIr5WoHw="
+          alt="building"
+        />
+      </CarouselSlider>
     </div>
   );
 }
---
+++ src/components/CarouselSlider.jsx
@@ -0,0 +1,69 @@
+import { useState, useEffect } from "react";
+
+import { FaArrowCircleLeft, FaArrowCircleRight } from "react-icons/fa";
+
+function CarouselSlider({ children }) {
+  const [current, setCurrent] = useState(0);
+  const [isButtonClicked, setButtonClicked] = useState(false);
+
+  const length = children.length;
+
+  // useEffect(() => {
+  //   setButtonClicked(false);
+  //   console.log("useEffect affected");
+  // }, [isButtonClicked]);
+
+  console.log(isButtonClicked);
+
+  function handleLeftArrow() {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+    console.log(current);
+    setButtonClicked(false);
+    setButtonClicked(true);
+  }
+
+  function handleRightArrow() {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+    console.log(current);
+  }
+
+  function renderCarousel(children) {
+    let leftSideImageIndex = current === 0 ? length - 1 : current - 1;
+    let rightSideImageIndex = current === length - 1 ? 0 : current + 1;
+    return (
+      <>
+        <div className="imageDiv left-image">
+          {children.map((item, index) =>
+            leftSideImageIndex === index ? item : null
+          )}
+        </div>
+        <div className="imageDiv front-image">
+          {children.map((item, index) => (current === index ? item : null))}
+        </div>
+        <div
+          className={
+            isButtonClicked
+              ? "right-image imageDiv right-image-animation"
+              : "imageDiv right-image "
+          }
+        >
+          {children.map((item, index) =>
+            rightSideImageIndex === index ? item : null
+          )}
+        </div>
+      </>
+    );
+  }
+
+  // console.log(children);
+  return (
+    <div className="container">
+      <FaArrowCircleLeft className="arrow" onClick={handleLeftArrow} />
+      <div className="carousel">{renderCarousel(children)}</div>
+      <FaArrowCircleRight className="arrow" onClick={handleRightArrow} />
+      <div className="simple-div">Click Me</div>
+    </div>
+  );
+}
+
+export default CarouselSlider;
