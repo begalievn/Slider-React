471757542fc4d698f682982bce048a279d625aee
begalievn <begaliev.info@gmail.com>
2022-05-17 16:22:34 +0600

    stable version

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ function App() {
   return (
     <div className="App">
       <CarouselSlider>
-        <img
-          className="image"
-          src="https://media.istockphoto.com/photos/exterior-view-of-modern-apartment-building-offering-luxury-rental-in-picture-id1322575582?b=1&k=20&m=1322575582&s=170667a&w=0&h=bGCtLpgCEorQuVdW2lbWguNZHcOGPePSwDibgbgyh0U="
-          alt="buildings"
-        />
+        <div className="image-wrapper">
+          <img
+            className="image"
+            src="https://media.istockphoto.com/photos/exterior-view-of-modern-apartment-building-offering-luxury-rental-in-picture-id1322575582?b=1&k=20&m=1322575582&s=170667a&w=0&h=bGCtLpgCEorQuVdW2lbWguNZHcOGPePSwDibgbgyh0U="
+            alt="buildings"
+          />
+          <p className="paragraph">First Image</p>
+        </div>
+
         <img
           className="image"
           src="https://media.istockphoto.com/photos/door-to-office-picture-id178842131?b=1&k=20&m=178842131&s=170667a&w=0&h=45iCO1nO7rvQS8NITsFmh4XQ_nIJfZ3-MVyP82ythdg="
@@ -23,6 +27,11 @@ function App() {
         <img
           className="image"
           src="https://media.istockphoto.com/photos/green-building-picture-id1329757808?b=1&k=20&m=1329757808&s=170667a&w=0&h=lhgX-lhkYxeHMJ5yM8X4DjpkR863NC_DFRP3FdU95r8="
+          alt="building"
+        />
+        <img
+          className="image"
+          src="https://images.unsplash.com/photo-1486175060817-5663aacc6655?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGJ1aWxkaW5nc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60"
           alt="building"
         />
         <img
--- src/components/CarouselSlider.jsx
+++ src/components/CarouselSlider.jsx
@@ -3,24 +3,75 @@ import { useState, useEffect } from "react";
 import { FaArrowCircleLeft, FaArrowCircleRight } from "react-icons/fa";
 
 function CarouselSlider({ children }) {
-  const [activeIndex, setActiveIndex] = useState(2);
+  const [activeIndex, setActiveIndex] = useState(0);
+  const length = children.length;
+
+  // useEffect(() => {
+  //   if (children === undefined) {
+  //     return;
+  //   }
+
+  //   Array.isArray(children)
+  //     ? setImages([...children])
+  //     : setImages([...images, children]);
+
+  //   for (let i = images.length; i < 6; i++) {
+  //     setImages((prevState) => [...prevState, ...prevState]);
+  //   }
+  // }, []);
+
+  let leftIndex = activeIndex ? activeIndex - 1 : length - 1;
+  let rightIndex = activeIndex === length - 1 ? 0 : activeIndex + 1;
+
+  let hidedLeftIndex = leftIndex ? leftIndex - 1 : length - 1;
+  let hidedRightIndex = rightIndex === length - 1 ? 0 : rightIndex + 1;
 
   function next() {
-    setActiveIndex(activeIndex + 1);
+    setActiveIndex(activeIndex === length - 1 ? 0 : activeIndex + 1);
   }
 
   function prev() {
-    setActiveIndex(activeIndex - 1);
+    setActiveIndex(activeIndex ? activeIndex - 1 : length - 1);
   }
+
+  console.log(
+    hidedLeftIndex,
+    leftIndex,
+    activeIndex,
+    rightIndex,
+    hidedRightIndex
+  );
 
   return (
     <div className="container">
       <FaArrowCircleLeft className={`arrow`} onClick={prev} />
       <div className="inner-container">
         <div className="carousel">
-          <div className={`imageDiv left`}>{children[activeIndex - 1]}</div>
-          <div className={`imageDiv front active`}>{children[activeIndex]}</div>
-          <div className={`imageDiv right`}>{children[activeIndex + 1]}</div>
+          {/* */}
+          <div
+            id="hidedLeft"
+            key={hidedLeftIndex}
+            className={`imageDiv hidedLeft`}
+          >
+            {children[hidedLeftIndex]}
+          </div>
+          <div id="left" key={leftIndex} className={`imageDiv left`}>
+            {children[leftIndex]}
+          </div>
+          <div id="active" key={activeIndex} className={`imageDiv active`}>
+            {children[activeIndex]}
+          </div>
+          <div id="right" key={rightIndex} className={`imageDiv right`}>
+            {children[rightIndex]}
+          </div>
+          <div
+            id="hidedRight"
+            key={hidedRightIndex}
+            className={`imageDiv hidedRight`}
+          >
+            {children[hidedRightIndex]}
+          </div>
+          {/* */}
         </div>
       </div>
       <FaArrowCircleRight className={`arrow`} onClick={next} />
